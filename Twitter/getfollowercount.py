2f3e528f40f98df1050c092a7a17f66439af1400
vipassana <vipassana.me@gmail.com>
2015-10-26 00:51:48 -0400

    Adding script to scrape number of followers from twitter

--- Twitter/getfollowercount.py
+++ Twitter/getfollowercount.py
@@ -3,32 +3,35 @@ import pandas as pd
 import numpy as np
 from lxml import html
 import requests as req
+import re
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
-path =  r'/projects/open/SocMedia/tweets/'
-if not os.path.exists(path+'FollowerCount'):
-    os.mkdir(path+'FollowerCount', mode=0777)
+k_pattern = re.compile(r'K')
+m_pattern = re.compile(r'M')
 
-for file_name in os.listdir(path):
-    #df = pd.read_csv(path + str('csv_tweetsFeb_22_2015_14_37.csv'))
-    df = pd.read_csv(path + file_name)
-    columns = ['username', 'followers']
-    followers_df = pd.DataFrame(data=None, index=None, columns=columns)
+for file_name in os.listdir(os.readlink('tweets')):
 
+
+    df = pd.read_csv(os.readlink('tweets') + file_name)
+    followers = list()
+    print file_name
     for user in df['Username']:
         page = req.get('https://twitter.com/'+ str(user))
         tree = html.fromstring(page.text)
         n_followers = tree.xpath('//a[@data-nav="followers"]/*/text()')
         if n_followers:
-            print user, n_followers[1]
-            followers_df.append(pd.DataFrame(list(user, n_followers[1]), columns=columns))
-            print followers_df
+            n_followers[1] = re.sub(r',', '', n_followers[1])
+            if k_pattern.search(n_followers[1]):
+                n_followers[1] = re.sub(r'K', '', n_followers[1])
+                n_followers[1] = int( float(n_followers[1])*1000)
+            elif m_pattern.search(n_followers[1]):
+                n_followers[1] = re.sub(r'M', '', n_followers[1])
+                n_followers[1] = int(float(n_followers[1])*1000000)
+            else:
+                n_followers[1] = int(n_followers[1])
 
-
-
-
-
-
-
-
-
-#csv_tweetsFeb_22_2015_14_37.csv
+            followers.append([user, n_followers[1]])
+    columns = ['username', 'followers']
+    followers_df = pd.DataFrame(data = followers, columns=columns)
+    followers_df.to_csv(os.readlink('output') + "followers_count.csv", mode='a+')
