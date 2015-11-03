6ed1e7a9d7e1162ca0f14f5be135f273926e4928
vipassana <vipassana.me@gmail.com>
2015-11-03 10:36:16 -0500

    Adding comments

--- Twitter/getfollowercount.py
+++ Twitter/getfollowercount.py
@@ -1,3 +1,4 @@
+''' Script that scrapes number of followers from a twitter timeline '''
 import os
 import pandas as pd
 import numpy as np
@@ -5,22 +6,32 @@ from lxml import html
 import requests as req
 import re
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+
 plt.style.use('ggplot')
 
-k_pattern = re.compile(r'K')
-m_pattern = re.compile(r'M')
+def getHTML(username):
+    page = req.get('https://twitter.com/'+ str(username))
+    tree = html.fromstring(page.text)
+    return tree
 
-for file_name in os.listdir(os.readlink('tweets')):
+def writeFollowerCount(followers):
+    columns = ['username', 'followers']
+    followers_df = pd.DataFrame(data = followers, columns=columns)
+    followers_df.to_csv(os.readlink('output') + "followers_count.csv", mode='a+')
 
 
-    df = pd.read_csv(os.readlink('tweets') + file_name)
+def getFollowerCount(tweet_df): #change to html
+    k_pattern = re.compile(r'K')
+    m_pattern = re.compile(r'M')
+
     followers = list()
-    print file_name
-    for user in df['Username']:
-        page = req.get('https://twitter.com/'+ str(user))
-        tree = html.fromstring(page.text)
+    for user in tweet_df['Username']:
+        tree = getHTML(user)
+        #Extracts a list of title and value
         n_followers = tree.xpath('//a[@data-nav="followers"]/*/text()')
         if n_followers:
+
             n_followers[1] = re.sub(r',', '', n_followers[1])
             if k_pattern.search(n_followers[1]):
                 n_followers[1] = re.sub(r'K', '', n_followers[1])
@@ -32,6 +43,26 @@ for file_name in os.listdir(os.readlink('tweets')):
                 n_followers[1] = int(n_followers[1])
 
             followers.append([user, n_followers[1]])
-    columns = ['username', 'followers']
-    followers_df = pd.DataFrame(data = followers, columns=columns)
-    followers_df.to_csv(os.readlink('output') + "followers_count.csv", mode='a+')
+    return followers
+    #writeFollowerCount(followers)
+        #columns = ['username', 'followers']
+        #followers_df = pd.DataFrame(data = followers, columns=columns)
+        #followers_df.to_csv(os.readlink('output') + "followers_count.csv", mode='a+')
+
+
+
+if __name__ == '__main__':
+    # Read csv data from files
+    i=0
+    for file_name in os.listdir(os.readlink('tweets')):
+
+        if i > 1:
+            break
+
+        tweet_df = pd.read_csv(os.readlink('tweets') + file_name)
+        for user in tweet_df['Username']:
+            pool = Pool(8)
+            html_pages = getHTML(user)
+            followers = pool.map(getFollowerCount, html_pages)
+        writeFollowerCount(followers)
+        i=i+1
